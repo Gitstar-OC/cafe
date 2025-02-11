69e8e82a0d12b1b1d2683e35c418779e1a9378e4
OC <chandankarom07@gmail.com>
2025-02-12 01:21:34 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1>Vite </h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
