ce8e243d0609690ae493bb06b7572ba2e94590dd
OC <chandankarom07@gmail.com>
2025-02-12 22:25:40 +0530

    Updated footer and json file

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="w-full bg-[#17E3EC] py-8 text-center">
-      <p className="text-lg">© 2023 Hack Club. All rights reserved.</p>
+      <p className="text-lg">©Hack Club. All rights reserved.</p>
     </footer>
   )
 }
